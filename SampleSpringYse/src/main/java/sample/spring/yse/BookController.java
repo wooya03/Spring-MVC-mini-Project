5a558f66ba149a91282378de5041e79d66b4ce5a
Wooya <wooyya03@naver.com>
2023-09-22 13:45:05 +0900

    브라우저를 비롯한 클라이언트(client)의 요청(request)을 받아서 응답(response)을 만들어내는 컨트롤러(controller) 클래스

--- SampleSpringYse/src/main/java/sample/spring/yse/BookController.java
+++ SampleSpringYse/src/main/java/sample/spring/yse/BookController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 // 브라우저를 비롯한 클라이언트(client)의 요청(request)을 받아서 응답(response)을 만들어내는 컨트롤러(controller) 클래스
-
 @Controller //컨트롤러 어노테이션(Annotation)을 설정
 public class BookController {
 	// 브라우저 주소가 /create일 때 실행되는 자바 컨트롤러 메소드
